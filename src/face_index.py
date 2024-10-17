a6cd1b5d9c6a4f61d0818bffb4e9cf11720d8c3d
mujeeb-gh <olamidebalogun174@gmail.com>
2024-10-17 18:48:42 +0100

    Add api

---
+++ src/face_index.py
@@ -0,0 +1,22 @@
+import cv2
+from deepface import DeepFace
+from typing import List, Literal
+from pymilvus import MilvusClient
+
+# Set up Milvus client
+# milvus_client = MilvusClient(host='localhost', port='19530', db_name='mnemosyne_face_db.db')
+
+
+
+# Get face embedding
+def get_face_embedding(img_path, model_name: Literal['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib']) -> List[float]:
+  face_embedding = DeepFace.represent(img_path, model_name=model_name, enforce_detection=True)
+  embedding_vector = face_embedding[0]['embedding']
+  embedding_dimension = len(embedding_vector)
+  print(f"Embedding dimension: {embedding_dimension}")
+  return embedding_vector
+
+img_path = 'assets\centralized_face.png'
+face_embedding = get_face_embedding(img_path, model_name='Facenet')
+
+# print(face_embedding)
