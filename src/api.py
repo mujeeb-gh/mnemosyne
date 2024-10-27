f92495b55c0b7ba187086646d6922e7f9f0a507a
mujeeb-gh <olamidebalogun174@gmail.com>
2024-10-27 12:34:06 +0100

    Add IP webcam selection

--- src/api.py
+++ src/api.py
@@ -14,13 +14,17 @@ app = Flask(__name__)
 # Initialize the camera
 cap = cv2.VideoCapture(0)
 
-def generate_frames():
+def generate_frames(video_source=0):
+    if isinstance(video_source, str):
+        cap = cv2.VideoCapture(video_source)
+    else:
+        cap = cv2.VideoCapture(int(video_source))
     while True:
         success, frame = cap.read()
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            _, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -117,7 +121,8 @@ def capture():
 
 @app.route('/video')
 def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    video_source = request.args.get('source', default=0)
+    return Response(generate_frames(video_source), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/static/script.js
+++ src/static/script.js
@@ -34,4 +34,39 @@ const captureImage = () => {
       statusDiv.className = "error";
       statusDiv.textContent = "Error: " + error.message;
     });
-}
+};
+
+const changeVideoSource = () => {
+  const videoSourceSelect = document.getElementById("videoSourceSelect");
+  const ipWebcamUrlInput = document.getElementById("ipWebcamUrl");
+  const video = document.getElementById("video");
+
+  if (videoSourceSelect.value === "ip") {
+    ipWebcamUrlInput.style.display = "block";
+    video.setAttribute("src", "");
+    const ipWebcamUrl = ipWebcamUrlInput.value;
+    if (ipWebcamUrl) {
+      video.setAttribute("src", `/video?source=${encodeURIComponent(ipWebcamUrl)}&t=${Date.now()}`);
+    }
+  } else if (videoSourceSelect.value === "default") {
+    ipWebcamUrlInput.style.display = "none";
+    video.setAttribute("src", `/video?t=${Date.now()}`); // This will use the default camera (source=0)
+  }
+};
+
+// Handle IP webcam URL changes
+document.getElementById("ipWebcamUrl").addEventListener("input", () => {
+  const videoSourceSelect = document.getElementById("videoSourceSelect");
+  if (videoSourceSelect.value === "ip") {
+    const ipWebcamUrl = document.getElementById("ipWebcamUrl").value;
+    const video = document.getElementById("video");
+    if (ipWebcamUrl) {
+      video.src = `/video?source=${ipWebcamUrl}`;
+    }
+  }
+});
+
+// Add event listener for video source selection changes
+document
+  .getElementById("videoSourceSelect")
+  .addEventListener("change", changeVideoSource);
